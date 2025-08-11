ab8eada6f00706be870d954bed6398388a71d582
Nurali11 <boltaboyevnurali55@gmail.com>
2025-08-11 23:42:06 +0300

    qwertyu

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -51,19 +51,19 @@ const Home = () => {
 
           </div>
           <strong className="font-semibold text-[16px]">
-            {isLoading ? <Skeleton.Input size={"default"} />: sellerData?.name}
+            {isLoading ? <Skeleton.Input active size={"default"} />: sellerData?.name}
           </strong>
         </div>
         <button
           className="bg-[#5F5F5] hover:!border-[#75CDB] calendar-button duration-300 !w-[40px] !text items-center justify-center !p-0 !rounded-[12px] !h-[40px]"
           onClick={() => {}}
         >
-          {!sellerData ? <Skeleton.Node style={{ width: 40, height: 40, borderRadius: 12}} /> : <Link to={PATH.kalendar}><div className="bg-[#F5F5F5] border-[2px] border-[#d9d5d5] cursor-pointer w-[40px] h-[40px] flex items-center rounded-[10px] justify-center"><CalendarIcon /></div></Link> }
+          {!sellerData ? <Skeleton.Node active style={{ width: 40, height: 40, borderRadius: 12}} /> : <Link to={PATH.kalendar}><div className="bg-[#F5F5F5] border-[2px] border-[#d9d5d5] cursor-pointer w-[40px] h-[40px] flex items-center rounded-[10px] justify-center"><CalendarIcon /></div></Link> }
           
         </button>
       </div>
 
-      {!sellerData ? <Skeleton.Node style={{ width: 360, height: 90, marginBottom: 31, borderRadius: 20}} /> : <div className="rounded-[20px] flex justify-center items-center relative bg-[#30AF49] mb-[31px] py-[18px] text-center">
+      {!sellerData ? <Skeleton.Node active style={{ width: 360, height: 90, marginBottom: 31, borderRadius: 20}} /> : <div className="rounded-[20px] flex justify-center items-center relative bg-[#30AF49] mb-[31px] py-[18px] text-center">
         <div className="flex flex-col">
           <strong className="font-bold text-white text-[20px]">
             {showLimit ? "*********" : `${isLoading ? `` : `${formatNumber(totalDebt)} so‘m`}`}
@@ -79,14 +79,14 @@ const Home = () => {
       </div>}
       
       <div className="flex justify-between mb-[40px]">
-        {!sellerData ? <Skeleton.Node style={{ width: 160, height: 120, borderRadius: 16}} /> : <div className="p-[16px] w-[48%] border-[1px] border-[#ECEC] rounded-[16px]">
+        {!sellerData ? <Skeleton.Node active style={{ width: 160, height: 120, borderRadius: 16}} /> : <div className="p-[16px] w-[48%] border-[1px] border-[#ECEC] rounded-[16px]">
           <div className="w-[110px] space-y-[20px]">
             <Text classList="!font-semibold !text-[18px]">Kechiktirilgan to‘lovlar</Text>
             <Text classList="!text-[18px] !text-[#F94D4D]">{sellerData?.Nasiya.length}</Text>
           </div>
         </div>}
 
-        {!sellerData ? <Skeleton.Node style={{ width: 160, height: 120, borderRadius: 16}} /> : <div className="p-[16px] w-[48%] border-[1px] border-[#ECEC] rounded-[16px]">
+        {!sellerData ? <Skeleton.Node active style={{ width: 160, height: 120, borderRadius: 16}} /> : <div className="p-[16px] w-[48%] border-[1px] border-[#ECEC] rounded-[16px]">
           <div className="w-[110px] space-y-[20px]">
             <Text classList="!font-semibold !text-[18px]">Mijozlar soni</Text>
             <Text classList="!text-[18px] !text-[#30AF49]">{sellerData?.Debtor.length}</Text>
@@ -95,7 +95,7 @@ const Home = () => {
 
       </div>
 
-      {!sellerData ? <Skeleton.Node style={{ width: 340, height: 150, borderRadius: 16}}/>: 
+      {!sellerData ? <Skeleton.Node active style={{ width: 360, height: 150, borderRadius: 16}}/>: 
       <>
       <div className="mb-[20px]">
         <Heading tag="h2" children="Hamyoningiz" classList="!text-[18px] !font-semibold !mb-[20px]"/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Home from "./dashboard/Home"
 
 const Login = lazy(() => new Promise((resolve:any) => {
     resolve(import("./auth/Login"))
-    // return setTimeout(() => resolve(import("./auth/Login")), 1500)
+    return setTimeout(() => resolve(import("./auth/Login")), 1500)
 }))
 
 export {Login, Home, LoginHome}
